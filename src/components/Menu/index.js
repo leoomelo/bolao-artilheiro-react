f28be000f0c8a35bcfbebbba0921a70446e1e29d
Leonardo Melo <leoomelo@gmail.com>
2021-03-03 16:02:04 -0300

    add menu and css

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 const MenuStyled = styled.div`
     height: 20vh;
     width: 100%;
+    background: #3C99DC;
+    color: white;
+    // background: rgb(60,153,220);
+    // background: linear-gradient(180deg, rgba(60,153,220,1) 0%, rgba(102,211,250,1) 55%, rgba(255,255,255,1) 100%);
 `
 function Menu() {
     return(
         <MenuStyled>
-            Brasileirão
+            Olá, Fulano
         </MenuStyled>
     )
 }
